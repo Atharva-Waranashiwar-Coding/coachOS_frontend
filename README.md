41e566069c9004b578fb95ce121848fc1336edf1
Atharva Waranashiwar <waranashiwaratharva@gmail.com>
2026-07-09 14:56:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# CoachOS Frontend
+
+Frontend application for CoachOS.
+
+## Local development
+
+```bash
+npm install
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <main className="app-shell">
+      <h1>CoachOS</h1>
+    </main>
+  );
+}
+
+export default App;
